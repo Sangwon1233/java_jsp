3ad403926f767c197e3ad1038a671aaf76df98b4
Sangwon1233 <tkddnjs3351@naver.com>
2024-11-01 18:21:17 +0900

    회원가입 데이터 및 회원가입ui

---
+++ 01_servlet/src/main/java/stud/MappingServlet.java
@@ -0,0 +1,21 @@
+package stud;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MappingServlet extends HttpServlet{
+
+	@Override //이걸해야 세션을 쓸수있음
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(req, resp);
+		System.out.println("mapping");
+	}
+	
+	
+
+}
---
+++ 01_servlet/src/main/java/stud/Testservlet.java
@@ -0,0 +1,41 @@
+package stud;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Testservlet
+ */
+@WebServlet("/Testservlet") //기본 생성자
+public class Testservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Testservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath()); //get은 키 벨류
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response); //post는 노출되면 안되는것들 그리고 긴거?
+	}
+
+}
